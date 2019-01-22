80590d27c693619ce2f9eded562b893361300566
Aaron Hedges <aaron.hedges@gmail.com>
2019-01-22 15:00:23 +0100

    add new graph class

--- app.js
+++ app.js
@@ -36,7 +36,78 @@ class Queue
             str += this.items[i] +" "; 
         return str; 
     } 
-} 
+}
+
+// new graph class to handle mappping
+class Graph {
+    constructor(noOfVertices) { 
+        this.noOfVertices = noOfVertices; 
+        this.AdjList = new Map(); 
+        this.curMapDirection = null;
+    } 
+  
+    addVertex(v) { 
+        this.AdjList.set(v, []); 
+    } 
+
+    addEdge(v, w) { 
+        this.AdjList.get(v).push(w); 
+        this.AdjList.get(w).push(v); 
+    }
+
+    printGraph() { 
+        let get_keys = this.AdjList.keys(); 
+      
+        console.log('printGraph: ');
+        for (var i of get_keys) { 
+            let get_values = this.AdjList.get(i); 
+            let conc = ""; 
+      
+            for (var j of get_values) 
+                conc += j + " "; 
+      
+            console.log(i + " -> " + conc); 
+        } 
+    }  
+  
+    bfs(startingNode, stepCount) {
+        console.log('bfs function start');
+        console.log('======================');
+        console.log('startingNode: ', startingNode);
+        let currentStep = 0;
+        console.log('currentStep: ', currentStep);
+        console.log('stepCount: ', stepCount);
+      
+        let visited = []; 
+        for (var i = 0; i < this.noOfVertices; i++) 
+            visited[i] = false; 
+      
+        let q = new Queue(); 
+      
+        visited[startingNode] = true; 
+        q.enqueue(startingNode); 
+
+        while (!q.isEmpty()) { 
+            let getQueueElement = q.dequeue();
+      
+            console.log('getQueueElement: ', getQueueElement); 
+      
+            // get adjacent list for current vertex 
+            let get_List = this.AdjList.get(getQueueElement); 
+            console.log('get_List: ', get_List);
+      
+            // loop to only process new vertexes 
+            for (var i in get_List) { 
+                var neigh = get_List[i]; 
+      
+                if (!visited[neigh]) { 
+                    visited[neigh] = true; 
+                    q.enqueue(neigh); 
+                } 
+            } 
+        } 
+    }
+}
 
 // quick and direct single page for testing
 const server = http.createServer((req, res) => {
