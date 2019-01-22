ffb721769d9ed55609984a4735cde2f48245502d
Aaron Hedges <aaron.hedges@gmail.com>
2019-01-22 14:59:11 +0100

    add que class for later functions

--- app.js
+++ app.js
@@ -3,6 +3,41 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+// quick and dirty que for this app
+class Queue 
+{ 
+    constructor() { 
+        this.items = []; 
+    } 
+                  
+    enqueue(element) {     
+        this.items.push(element); 
+    } 
+    
+    dequeue() { 
+        if(this.isEmpty()) 
+            return "Underflow"; 
+        return this.items.shift(); 
+    } 
+
+    front() { 
+        if(this.isEmpty()) 
+            return "No elements in Queue"; 
+        return this.items[0]; 
+    }  
+
+    isEmpty() { 
+        return this.items.length == 0; 
+    } 
+
+    printQueue() { 
+        var str = ""; 
+        for(var i = 0; i < this.items.length; i++) 
+            str += this.items[i] +" "; 
+        return str; 
+    } 
+} 
+
 // quick and direct single page for testing
 const server = http.createServer((req, res) => {
   const startDigit = '5';
